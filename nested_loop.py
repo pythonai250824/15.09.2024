17fa55a73c3555e570151a8d6fec1896de19df5c
fullstack290623 <fullstack290623@gmail.com>
2024-09-15 21:05:37 +0300

    upload

---
+++ nested_loop.py
@@ -0,0 +1,38 @@
+import random
+# input 5 height from the user
+#for _ in range(1, 5 + 1):
+# sum_heights: float = 0.0
+# for _ in range(1, 5 + 1):  # 0 1 2 3 4
+#     # input height , until the height is between 0.4-2.5
+#     height: float = float(input('enter height: '))
+#     while not 0.4 <= height <= 2.5:
+#         height = float(input('enter height: '))
+#     else:
+#         print(f"{height} is correct.")
+#     sum_heights += height
+#
+# print(f"average = {sum_heights / 5}")
+#
+# """
+# 1: 12345
+# 2: 12345
+# 3: 12345
+# """
+# for i in range(3):
+#     for i in range(1, 5 + 1):
+#         print(i, end = " ")
+#     print()
+
+#4
+# * * * *
+# * * * *
+# * * * *
+# * * * *
+
+#4
+# *
+# * *
+# * * *
+# * * * *
+
+
--- randoms.py
+++ randoms.py
@@ -6,8 +6,51 @@ for _ in range(10):
 print()
 print(random.randint(100, 1000))
 
+choice: int = random.randint(1, 2)
+if choice == 1:
+    print(True)
+else:
+    print(False)
+
+#                                                       1     2      3    4
+print("random.choice([True, False]): ", random.choice( [True, False, [] , 2.3] ))
+
+# random and print: a day "Sunday", "Monday" ...
+day: str = random.choice( ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] )
+print(f'random day is {day}')
+
 print(f"{random.uniform(9.5, 10.7):.2f}")
 
 print(f"integer: {random.randint(10, 20):.2f}")
 print(f"{random.uniform(10.5, 20):.2f}")
+# num1: int = random.randint ...
+# create a random first number between 0-100
+# create a random second number between 0-100
+# {number1} + {number2} = ?
+# 30 + 70 = ?
+# 100
+# correct
+# 90
+# wrong ...
+
+num1: int = random.randint(0, 100)
+num2: int = random.randint(0, 100)
+sum_num: int = num1 + num2
+answer = int(input(f"{num1} + {num2} = ?"))
+if answer != sum_num:
+    print('wrong...')
+else:
+    print('correct!')
+
+# extra -- loop
+num1: int = random.randint(0, 100)
+num2: int = random.randint(0, 100)
+sum_num: int = num1 + num2
+answer: int = int(input(f"{num1} + {num2} = ?"))
+while answer != sum_num:
+    print('wrong...')
+    answer = int(input(f"{num1} + {num2} = ?"))
+else:
+    print('correct!')
+
 
